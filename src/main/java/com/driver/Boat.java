84af98adf509c77213f848291e0563f15b82e4ac
Shriniwas Tak <shriniwastak@gmail.com>
2023-08-13 20:09:41 +0530

    Vehicle

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -4,32 +4,27 @@ public class Boat implements WaterVehicle{
     private String name;
     private int capacity;
 
-    public Boat(String name,int capacity){
-        this.name=name;
-        this.capacity=capacity;
+    public Boat() {
     }
 
-    public String getName() {
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    public String getVehicleName() {
         return name;
+    }
+
+    public int getVehicleCapacity() {
+        return capacity;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public int getCapacity() {
-        return capacity;
-    }
-
     public void setCapacity(int capacity) {
         this.capacity = capacity;
-    }
-
-    @Override
-    public String getVehicleName(){return this.name;}
-
-    @Override
-    public int getVehicleCapacity(){
-        return this.capacity;
     }
 }
